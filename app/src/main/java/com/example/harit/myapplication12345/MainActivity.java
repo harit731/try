9a677d1596ed567a295c687208b06df9db82e19e
harit731 <chandra.harit@gmail.com>
2016-02-27 17:19:08 +0530

    try

---
+++ app/src/main/java/com/example/harit/myapplication12345/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.harit.myapplication12345;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.CountDownTimer;
+import java.lang.Object;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    new CountDownTimer(30000,1000) {
+
+        public void onTick ( long millisUntilFinished){
+//            mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            System.out.println("heuy");
+        }
+
+    public void onFinish() {
+        //      mTextField.setText("done!");
+    }
+}.start();
+        }
+
